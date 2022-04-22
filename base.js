b22b56d77b32560e42c607d056f9430ec3b06387
Neilson Venute <neilsonvenute@gmail.com>
2022-04-22 20:05:51 -0300

    Quantificadores

--- base.js
+++ base.js
@@ -3,12 +3,24 @@ João trouxe    flore para sua amada namorada em 10 de janeiro de 1970,
 Maria era o nome dela.
 
 
-Foi um ano excelente na vida de joão. Teve 5 filhos, todos adultos atualmente. 
-maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de 
+Foi um ano excelente na vida de joão. Teve 5 filhos, todos adultos atualmente.
+maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de
 domingo. Também né! Sendo a boa mineira que é, nunca esquece seu famoso pão de queijo.
 Não canso de ouvir a Maria:
 "Jooooooooooãooooooooooo, o café tá prontinho aqui. Veeemmm!"
 `
+
+const arquivos = [
+  'Atenção.jpg',
+  'FOTO.jpeg',
+  'Meu gatinho.jpg',
+  'Meu gatinho.JPG',
+  'Meu gatinho.JPEG',
+  'Meu gatinho.JPeeEEEEeeeeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEG',
+  'Marido.png',
+  'lista de compras.txt',
+]
+
 module.exports = {
-  texto
+  texto,arquivos
 }
