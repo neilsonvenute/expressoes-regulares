5cda0274502081d3882152abff19aa944c518e00
Neilson Venute <neilsonvenute@gmail.com>
2022-04-23 09:15:18 -0300

    lookahead e lookbehind (lookaround)

--- base.js
+++ base.js
@@ -55,7 +55,15 @@ const ips = `
 
 const alfabeto = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ atenção abcdefghijklmnopqrstuvwxyz ®i 1=0123456789 i'
 
+const lookahead = `ONLINE 192.168.0.1 ABCDEF inactive
+OFFLINE 192.168.0.2 ABCDEF active
+ONLINE 192.168.0.3 ABCDEF active
+ONLINE 192.168.0.4 ABCDEF active
+OFFLINE 192.168.0.5 ABCDEF active
+OFFLINE 192.168.0.6 ABCDEF inactive
+`
+
 
 module.exports = {
-  texto,arquivos,html,alfabeto,cpfs,ips,cpfs2,html2
+  texto,arquivos,html,alfabeto,cpfs,ips,cpfs2,html2,lookahead
 }
